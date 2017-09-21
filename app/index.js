d78da0d901675470300c0864f29176d73b2539ed
karthikbhat6 <karbhat6@gmail.com>
2017-09-21 14:12:49 +0530

    no express

--- app/index.js
+++ app/index.js
@@ -1,9 +1,24 @@
+import { BrowserRouter } from 'react-router-dom'
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/App');
 /*var express = require('express');
 var app = express();
+var path = require('path');*/
+/*
+app.set('views', __dirname + '/components');
+app.set('view engine', 'js');
+app.engine('js', require('express-react-views').createEngine());
 
+app.get('/', function(request, response) {
+   ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+	);
+    
+});
+app.listen(8080);*/
+/*
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -13,15 +28,37 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  //response.render('pages/index');
- 
+  response.render('pages/index');
+    
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});*/
+
+/*// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+	console.log('sent html');
+    //res.sendFile(path.join(__dirname + 'index.html'));
+   res.sendFile(path.join(__dirname + 'check.html'));
+     
 });
-*/
- ReactDOM.render(
+
+app.listen(8080);*/
+
+ /*ReactDOM.render(
 	<App />,
 	document.getElementById('app')
 	);
+*/
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('app'));
+
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports={
 		}
 		]
 	},
+	 
 	output:{
 		filename: 'transformed.js',
 		path: __dirname + '/build'
