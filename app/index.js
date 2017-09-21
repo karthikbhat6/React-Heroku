ff615a39ba7057b2bd2b0937c1f599756fb82819
karthikbhat6 <karbhat6@gmail.com>
2017-09-21 09:22:59 +0530

    Third commit

--- app/index.js
+++ app/index.js
@@ -1,9 +1,27 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/App');
+/*var express = require('express');
+var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 
-ReactDOM.render(
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  //response.render('pages/index');
+ 
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+*/
+ ReactDOM.render(
 	<App />,
 	document.getElementById('app')
 	);
